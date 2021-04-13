b4e2d376c70dc8573dcf9d41d3350cee6d20ad86
iMac21.5-2012 <61crossroad@gmail.com>
2021-04-14 00:24:01 +0900

    use custom plugin instead of mixin

--- components/MessageListItem.js
+++ components/MessageListItem.js
@@ -1,8 +1,8 @@
-import lifecycleLogger from '../mixins/lifecycle-logger.mixin.js';
+// import lifecycleLogger from '../mixins/lifecycle-logger.mixin.js';
 
 export default {
     name: 'MessageListItem',
-    mixins: [lifecycleLogger],
+    // mixins: [lifecycleLogger],
     template: `<li>{{ item.text }} - {{ item.createdAt | datetime }}
     <button @click="deleteClicked">X</button></li>`,
     props: {
